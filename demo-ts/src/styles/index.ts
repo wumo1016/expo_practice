1bbb35bd6467ecbc851e3126fc789ed5a860b10d
wumo1016 <760478279@qq.com>
2024-04-09 13:28:00 +0800

    feat: 公共样式配置

---
+++ demo-ts/src/styles/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Description: 全局样式
+ * @Author: wyb
+ * @LastEditors: wyb
+ * @LastEditTime: 2024-04-09 13:27:15
+ */
+import { StyleSheet } from 'react-native'
+
+export const globalStyle = StyleSheet.create({})
---
+++ demo-ts/src/styles/theme.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: wyb
+ * @LastEditors: wyb
+ * @LastEditTime: 2024-04-09 13:23:01
+ */
+export default {
+  // 文字色
+  // 背景色
+  // 边框色
+  // 圆角
+}
